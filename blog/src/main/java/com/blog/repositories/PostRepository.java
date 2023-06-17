de830d6c30232271d0c37c0e053bc8b812930c07
Gautam kumar <127440256+Gk4244@users.noreply.github.com>
2023-06-18 00:23:24 +0530

    Delete blog directory

--- blog/src/main/java/com/blog/repositories/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.blog.repositories;
-
-import com.blog.entites.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post,Long> {
-}
